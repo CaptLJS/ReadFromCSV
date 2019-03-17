e11d580da5ea9f1cca26d0cad3ce51b3dffa7765
CaptLJS <47562344+CaptLJS@users.noreply.github.com>
2019-03-18 01:15:38 +0530

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClientSide
+{
+
+    using ServerSide;
+    class MainClass
+    {
+         static void Main(string[] args)
+        {
+           
+            int n;
+            Console.WriteLine("Enter 1 for viewing employee by Location");
+            Console.WriteLine("Enter 2 for viewing employee by Date of birth");
+            Console.WriteLine("Enter 3 for viewing employee by Designation");
+            n = int.Parse(Console.ReadLine());
+            if(n==1)
+            {
+                Console.WriteLine("Enter location");
+                string l = Console.ReadLine();
+                MyClass.Location(l);
+            }
+            else if(n==2)
+            {
+                Console.WriteLine("Enter DOB");
+                string dob = Console.ReadLine();
+                MyClass.DOB(dob);
+            }
+            else if(n==3)
+            {
+                Console.WriteLine("Enter designation");
+                string d = Console.ReadLine();
+                MyClass.Designation(d);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect entry");
+            }
+
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace ServerSide
+{
+    public class MyClass
+    {
+
+        public static void Location(string location)
+        {
+            var reader = new StreamReader(File.OpenRead(@"/Users/ritwik/Downloads/100 Records.csv"));
+            var test = true;
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                if (values[29] == location)
+                {
+                    Console.WriteLine(location);
+                    Console.WriteLine(values[2] + " " + values[3] + " " + values[4]);
+                    test = false;
+                }
+
+                Console.WriteLine();
+
+            }
+
+            if(test)
+
+            {
+                Console.WriteLine("No such location");
+            }
+
+
+        }
+
+
+
+        public static void DOB(string dob)
+        {
+            var reader = new StreamReader(File.OpenRead(@"/Users/ritwik/Downloads/100 Records.csv"));
+            DateTime dob2 = Convert.ToDateTime(dob);
+            var test = true;
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                if (values[10] == dob)
+
+                    {
+                        Console.WriteLine(dob);
+                        Console.WriteLine(values[2] + " " + values[3] + " " + values[4]);
+                        test = false;
+                    }
+
+                Console.WriteLine();
+
+            }
+            if(test)
+            {
+                Console.WriteLine("No such date of birth");
+            }
+
+
+        }
+
+
+
+        public static void Designation(string designation)
+        {
+            var reader = new StreamReader(File.OpenRead(@"/Users/ritwik/Downloads/100 Records.csv"));
+            var test = true;
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+
+                    if (values[1] == designation)
+                    {
+                        Console.WriteLine(designation);
+                        Console.WriteLine(values[2] + " " + values[3] + " " + values[4]);
+                        test = false;
+                    }
+
+                Console.WriteLine();
+
+            }
+            if (test)
+            {
+                Console.WriteLine("No such designation");
+            }
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
